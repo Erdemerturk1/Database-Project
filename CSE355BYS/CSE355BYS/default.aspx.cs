b8e7e0437dc504438f35dea8bb9d047edc39e21a
Erdem Ertürk <117317579+erdemrtrk@users.noreply.github.com>
2025-12-26 20:14:47 +0300

    Delete CSE355BYS directory

--- CSE355BYS/CSE355BYS/default.aspx.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace CSE355BYS
-    public partial class _default : System.Web.UI.Page
-    {
-        protected void Page_Load(object sender, EventArgs e) { }
-    }
-}
--- CSE355BYS/CSE355BYS/template.Master.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace CSE355BYS
-{
-    public partial class template : System.Web.UI.MasterPage
-    {
-        protected void Page_Load(object sender, EventArgs e) { }
-    }
-}
